a035a008b015dcc4d776cc1db2da7d5243dba96c
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-02-10 18:00:29 +0000

    resized container div. now contains 16x16 divs with class cell.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // creates 256 divs (grid)
-for(i = 0; i < 256; i++){
+for (i = 0; i < 256; i++){
     const container = document.querySelector('#container');
     const div = document.createElement('div');
     div.classList.add("cell");
