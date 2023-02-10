af6d2c4cf20a07cf1c2d225cc50117b3dda1c6c1
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-02-10 22:55:34 +0000

    changed from flexbox to grid for grid creation

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// creates 256 divs (grid)
-for (i = 0; i < 256; i++){
-    const container = document.querySelector('#container');
-    const div = document.createElement('div');
-    div.classList.add("cell");
-    container.appendChild(div);
-    console.log(i);
+const container = document.querySelector("#container");
+let cell = document.querySelectorAll('.cell')
+window.onload = createGrid();
+
+function createGrid(){
+    for (let i = 0; i < 256; i++){
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        container.appendChild(div);
+    }
 }
+
+console.log(container);
+
+
+
+
 
