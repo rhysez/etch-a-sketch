ec2171d773d2b382d87e1c5acd12c9cc13b4d5b9
rhysez <121772484+rhysez@users.noreply.github.com>
2023-03-14 12:18:49 +0000

    add color selector, add color selector functions

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#container");
 const buttonSize = document.querySelector("#gridsize");
-const buttonRainbow = document.querySelector("#rainbow");
+const buttonRandom = document.querySelector("#random");
+
+const red = document.querySelector('#red');
+const blue = document.querySelector('#blue');
+const green = document.querySelector('#green');
+const yellow = document.querySelector('#yellow');
+const orange = document.querySelector('#orange');
+const purple = document.querySelector('#purple');
+const pink = document.querySelector('#pink');
+const white = document.querySelector('#white');
+
 let cell = document.querySelectorAll('.cell');
 let cellAmount = 16;
 window.onload = createGrid(cellAmount);
@@ -26,15 +36,93 @@ function mouseOver(){
     })
 }
 
-// changes cell colors to random RGB value
-// UNFINISHED
-function mouseOverRainbow(){
+function mouseOverRandom(){
     let cell = document.querySelectorAll('.cell');
     let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256))
                  + ',' + (Math.floor(Math.random() * 256)) + ')';
     cell.forEach((cell) => {
         cell.addEventListener('mouseover', function(){
             cell.style.background = color;
+        })
+    })
+}
+
+function mouseOverRed(){
+    let cell = document.querySelectorAll('.cell');
+    let colorRed = 'red';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorRed;
+        })
+    })
+}
+
+function mouseOverBlue(){
+    let cell = document.querySelectorAll('.cell');
+    let colorBlue = 'blue';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorBlue;
+        })
+    })
+}
+
+function mouseOverGreen(){
+    let cell = document.querySelectorAll('.cell');
+    let colorGreen = 'green';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorGreen;
+        })
+    })
+}
+
+function mouseOverYellow(){
+    let cell = document.querySelectorAll('.cell');
+    let colorYellow = 'yellow';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorYellow;
+        })
+    })
+}
+
+function mouseOverOrange(){
+    let cell = document.querySelectorAll('.cell');
+    let colorOrange = 'orange';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorOrange;
+        })
+    })
+}
+
+function mouseOverPurple(){
+    let cell = document.querySelectorAll('.cell');
+    let colorPurple = 'purple';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorPurple;
+        })
+    })
+}
+
+function mouseOverPink(){
+    let cell = document.querySelectorAll('.cell');
+    let colorPink = 'pink';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorPink;
+        })
+    })
+}
+
+function mouseOverWhite(){
+    let cell = document.querySelectorAll('.cell');
+    let colorWhite = 'white';
+    cell.forEach((cell) => {
+        cell.addEventListener('mouseover', function(){
+            cell.style.background = colorWhite;
         })
     })
 }
@@ -59,7 +147,39 @@ buttonSize.addEventListener('click', () => {
         mouseOver();
 });
 
-buttonRainbow.addEventListener('click', () => {
-    cell.onmouseover = mouseOverRainbow();
+buttonRandom.addEventListener('click', () => {
+    cell.onmouseover = mouseOverRandom();
+});
+
+red.addEventListener('click', () => {
+    cell.onmouseover = mouseOverRed();
+});
+
+blue.addEventListener('click', () => {
+    cell.onmouseover = mouseOverBlue();
+});
+
+green.addEventListener('click', () => {
+    cell.onmouseover = mouseOverGreen();
+});
+
+yellow.addEventListener('click', () => {
+    cell.onmouseover = mouseOverYellow();
+});
+
+orange.addEventListener('click', () => {
+    cell.onmouseover = mouseOverOrange();
+});
+
+purple.addEventListener('click', () => {
+    cell.onmouseover = mouseOverPurple();
+});
+
+pink.addEventListener('click', () => {
+    cell.onmouseover = mouseOverPink();
+});
+
+white.addEventListener('click', () => {
+    cell.onmouseover = mouseOverWhite();
 });
 
